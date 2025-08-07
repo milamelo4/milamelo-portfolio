934b837303cef63b4d29bf8d9af03615b8c01b2a
Camila Melo <milamelofl@yahoo.com>
2025-08-07 13:11:53 -0600

    Refactor Vite configuration for clarity and remove unnecessary server settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/milamelo-portfolio/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),    
-  ].filter(Boolean),
+export default defineConfig({
+  base: "/milamelo-portfolio/", // this is CRITICAL for GitHub Pages
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
